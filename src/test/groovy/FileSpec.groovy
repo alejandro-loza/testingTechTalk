b42ef13bb03ef7a581ce81ea004875585e67008c
pinks <pinkysauriohl@gmail.com>
2019-09-20 15:18:17 -0500

    added file test

--- src/test/groovy/FileSpec.groovy
+++ src/test/groovy/FileSpec.groovy
@@ -1,8 +1,10 @@
+import spock.lang.See
 import spock.lang.Specification
 
+@See("http://spockframework.org/spec")
 class FileSpec extends Specification {
 
-    def "Should create and clena up a file"() {
+    def "Should create and clean up a file"() {
         given:
         def file = new File('logs/app.log')
         file.createNewFile()
